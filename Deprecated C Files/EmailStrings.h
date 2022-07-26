95cbb73376313bae6e5716dad8c06c9ba57b43db
hjjba <hjb2677@g.rit.edu>
2022-07-26 19:28:35 -0400

    Removed C files

--- Deprecated C Files/EmailStrings.h
+++
@@ -1,7 +0,0 @@
-// MSD P22835
-// Themed Entertainment Model Display
-// File    : EmailStrings.h
-// Author  : Harrison Barnes
-// Data    : 04/05/2022
-// Purpose : Contains strings used in emailing TPE
-
