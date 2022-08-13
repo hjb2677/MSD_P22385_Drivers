0c46c3ef5312ce3c0afc5a1f2c6afdefbf6ae8e8
hjjba <hjb2677@g.rit.edu>
2022-08-13 13:40:05 -0400

    Fixed formatting and typo errors in ErrorReporter files

--- ErrorReport.py
+++ ErrorReport.py
@@ -10,3 +10,12 @@ Purpose : Header file for error reporter subroutine
 # All reports cleared - program can continue running
 ERROR_REPORTER_CONTINUE_DRIVER = 6400
 
+# Test Error - A simple code that is used for testing the email functionality and error reporting
+EMAIL_REPORTER_TEST_ERROR = 6666
+
+# Origin Address - the display's email addressed used to report the errors to the client
+EMAIL_ORIGIN_ADDRESS = "DisplayErrorReporterP22385@gmail.com"
+
+# Destination Address - the address which is supposed to receive the errors. this will be the client's email
+EMAIL_DRIVER_ADDRESS = ""
+
--- ErrorReporter.py
+++ ErrorReporter.py
@@ -10,7 +10,23 @@ Purpose : Driver program for the Error Reporting system
 # Includes
 import ErrorCodes
 import ErrorReport
+import smtplib
+from email.message import EmailMessage
 
 def ErrorReporterDriver():
+    errorCode = ErrorReport.EMAIL_REPORTER_TEST_ERROR
+    EmailErrorToClient(errorCode)
     return ErrorReport.ERROR_REPORTER_CONTINUE_DRIVER
+
+
+"""
+Function : EmailErrorToClient
+Inputs   : errorCode - the driver-determined error code associated with the identified error
+Outputs  : none
+
+Sends a formatted error notification email to the client. accompanied with other potentially
+useful information
+"""
+def EmailErrorToClient(errorCode):
+    pass
 
