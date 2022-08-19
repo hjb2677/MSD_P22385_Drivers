c28dfb2c894dd5d7fe98a1986c6a7e45dca1c7cc
hjjba <hjb2677@g.rit.edu>
2022-08-19 15:23:22 -0400

    Completed unit tests for Scheduler

--- UnitTest.py
+++ UnitTest.py
@@ -8,6 +8,10 @@ Purpose : Unit Test File - performs individual tests on the subroutines.
           Must pass tests for display to run correctly
 """
 
+# Imports
+import Scheduler
+import datetime
+
 # Subroutine defines - selects the correct subroutine to examine
 TEST_SCHEDULER = "SCH"
 TEST_IDLER = "IDL"
@@ -72,6 +76,39 @@ Tests the individual components of the Scheduler subroutine
 """
 def testScheduler():
     print("\n\tTesting Scheduler...")
+
+    # Check that fetch time and fetch date function correctly
+    # Note - ignores seconds when testing. Can fail is tested close to the change of a minute or hour due to latency
+    assert datetime.datetime.now().time().minute == Scheduler.FetchTime().minute, "FetchTime() incorrect - min"
+    assert datetime.datetime.now().time().hour == Scheduler.FetchTime().hour, "FetchTime() incorrect - hour "
+    assert datetime.date.today() == Scheduler.FetchDate(), "FetchDate() incorrect"
+
+    # Initialize test arrays - holds sets of dates and times to validate with
+    test_dates = [datetime.date.fromisoformat("2022-08-15"),    # Valid date, valid time
+                  datetime.date.fromisoformat("2022-08-15"),    # Valid date, invalid hour
+                  datetime.date.fromisoformat("2022-08-19"),    # Valid date, invalid minute
+                  datetime.date.fromisoformat("2022-08-20"),    # Invalid date, valid time
+                  datetime.date.fromisoformat("2023-08-21"),    # Invalid date, invalid time
+                  datetime.date.fromisoformat("2024-02-29"),    # Leap date, valid time
+                  datetime.date.fromisoformat("2024-02-29")]    # Leap date, invalid time
+
+    test_times = [datetime.time.fromisoformat("12:00:00"),      # Valid date, valid time
+                  datetime.time.fromisoformat("00:00:00"),      # Valid date, invalid hour
+                  datetime.time.fromisoformat("07:15:00"),      # Valid date, invalid minute
+                  datetime.time.fromisoformat("12:00:00"),      # Invalid date, valid time
+                  datetime.time.fromisoformat("03:00:00"),      # Invalid date, invalid time
+                  datetime.time.fromisoformat("12:00:00"),      # Leap date, valid time
+                  datetime.time.fromisoformat("04:00:00")]      # Leap date, invalid hour
+
+    # Initialize validation answer array - holds the expected validation values
+    test_validation = [0, -2, -1, -3, -2, 0, -2]
+
+    # Loop through all test cases and assert validation status
+    for index in range(len(test_validation)):
+        print("\t\tExamining test case : ", index)
+        assert test_validation[index] == Scheduler.ValidateTime(test_times[index], test_dates[index]), \
+            "Validation incorrect"
+
     print("\tDone testing Scheduler!")
 
 
