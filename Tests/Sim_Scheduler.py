a828269ab7bce6311ba29d8516b34014f45194ec
hjjba <hjb2677@g.rit.edu>
2022-09-09 23:15:32 -0400

    Created "Tests" directory to hold unit tests, simulations, and others tests and demos. Created a Sim_Scheduler.py file to simulate a few years of the scheduler

---
+++ Tests/Sim_Scheduler.py
@@ -0,0 +1,30 @@
+"""
+MSD P22385
+Themed Entertainment Model Display
+File    : Sim_Scheduler.py
+Author  : Harrison Barnes
+Date    : 09/09/2022
+Purpose : Simulates two years worth of time: Jan-01-2023 -> Jan-01-2025
+Calls the scheduler each day and prints data to a text file. The text file
+is formatted every month in order day. Does not test time validation, only date.
+All times are set to noon
+"""
+
+import Scheduler
+import Schedule
+import datetime
+
+
+"""
+Function : mainSimSchedule
+Inputs   : none
+Outputs  : none
+
+Simulates a period of time and checks scheduler. Prints validation code to a text file
+"""
+def mainSimSchedule():
+    pass
+
+
+if __name__ == '__main__':
+    mainSimSchedule()
