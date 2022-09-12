a9ee09445b0041b4efd76646b5eb93ea3bc62151
hjjba <hjb2677@g.rit.edu>
2022-09-12 09:27:16 -0400

    Added 2023 labor day to an invalid date in Schedule.py Sim_Scheduler.py now works full : prints the ISO date, DOW, and validation code to Sim_Scheduler_Results.txt

--- Tests/Sim_Scheduler.py
+++ Tests/Sim_Scheduler.py
@@ -17,12 +17,22 @@ import datetime
 
 # Defines
 
+# Time of day constant - noon
+TEST_TIME_SIM = "12:00:00"
+
+# Holds string representation of DOW
+DOW_STR = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+
 # Calendar years to simulate. Must include ALL year in between start and end
-YEARS_TO_SIM = [2022]
+YEARS_TO_SIM = [2023, 2024]
 
 # Array consisting of the number of days within a (leap) year
 DAYS_IN_MON = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 LEAP_DAYS_IN_MON = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+# Array contains month strings to print to output file
+MON_STR = ["\nJanuary\n", "\nFebruary\n", "\nMarch\n", "\nApril\n", "\nMay\n", "\nJune\n", "\nJuly\n", "\nAugust\n",
+           "\nSeptember\n", "\nOctober\n", "\nNovember\n", "\nDecember\n"]
 
 """
 Function : mainSimSchedule
@@ -32,6 +42,9 @@ Outputs  : none
 Simulates a period of time and checks scheduler. Prints validation code to a text file
 """
 def mainSimSchedule():
+    # Print start message to terminal
+    print("Beginning sim")
+
     # Open the simulation results file in write mode to prepare for sim output
     sim_file = open("Sim_Scheduler_Results.txt", "w")
 
@@ -41,18 +54,67 @@ def mainSimSchedule():
     sim_file.writelines(headerLines)
 
     for year in YEARS_TO_SIM:  # Iterate through all desired years
-        sim_file.write("\nNEW YEAR HERE\n")
-        for month in range(1, 12):  # Iterate through the 12 months in the year
-            sim_file.write("\nNEW MONTH HERE\n")
-            # Iterate through all the days within the month
-            # DAYS_IN_MONTH stores the amount of days within the respective month.
-            # "month-1" offset aligns month with correct index
-            # "+1" lets the loop terminate correctly
-            for day in range(1, DAYS_IN_MON[month-1]+1):
-                sim_file.write(datetime.date(year, month, day).strftime("%y-%m-%d\n"))
+        sim_file.write("\n\n")
+        sim_file.write(str(year))
+        sim_file.write("\n")
+        if IsLeapYear(year):
+            for month in range(1, 13):  # Iterate through the 12 months in the year
+                sim_file.write(MON_STR[month-1])
+                # Iterate through all the days within the month
+                # DAYS_IN_MONTH stores the amount of days within the respective month.
+                # "month-1" offset aligns month with correct index
+                # "+1" lets the loop terminate correctly
+                for day in range(1, LEAP_DAYS_IN_MON[month-1]+1):
+                    sim_file.writelines([datetime.date(year, month, day).strftime("%y-%m-%d: "),
+                                         DowToStr(datetime.date(year, month, day)),
+                                         " = ",
+                                         str(Scheduler.ValidateTime(datetime.time.fromisoformat(TEST_TIME_SIM),
+                                                                    datetime.date(year, month, day))), "\n"])
+                    sim_file.write("\n")
+        else:
+            for month in range(1, 13):  # Iterate through the 12 months in the year
+                sim_file.write(MON_STR[month-1])
+                # Iterate through all the days within the month
+                # DAYS_IN_MONTH stores the amount of days within the respective month.
+                # "month-1" offset aligns month with correct index
+                # "+1" lets the loop terminate correctly
+                for day in range(1, DAYS_IN_MON[month-1]+1):
+                    sim_file.writelines([datetime.date(year, month, day).strftime("%y-%m-%d: "),
+                                        DowToStr(datetime.date(year, month, day)),
+                                        " = ",
+                                        str(Scheduler.ValidateTime(datetime.time.fromisoformat(TEST_TIME_SIM),
+                                                                    datetime.date(year, month, day))), "\n"])
+                    sim_file.write("\n")
 
     # Close file. This ends write mode and frees device memory. DO NOT REMOVE
     sim_file.close()
+
+    # Print end sim message to terminal
+    print("Sim finished")
+
+
+"""
+Function : IsLeapYear
+Inputs   : year - an integer representation of the specified year
+Outputs  : boolean value - true if leap year, false otherwise
+
+Returns a boolean value that represents whether a year is a leap year in the Gregorian calendar system
+NOTE - Simplifies the gregorian calendar leap year rules to include every fourth year. Years like 2100, 2200, etc.
+are not leap years, but will be treated as such here. This is a non-issue since the display will not last 80 years
+"""
+def IsLeapYear(year):
+    return year % 4 == 0
+
+
+"""
+Function : DowToStr
+Inputs   : currDate - current date examined by the simulator
+Outputs  : string representation of the day (3 letters)
+
+Returns a DOW str representation for use to sim file output
+"""
+def DowToStr(currDate):
+    return DOW_STR[currDate.isocalendar()[2] - 1]
 
 
 if __name__ == '__main__':
