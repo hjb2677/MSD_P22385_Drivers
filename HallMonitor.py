7eafdc162022f903131b5f1189836e72cf189d80
hjjba <hjb2677@g.rit.edu>
2022-10-04 11:00:04 -0400

    Added Adafruit code for the ADC - ADS1015

--- HallMonitor.py
+++ HallMonitor.py
@@ -9,7 +9,22 @@ Purpose : Driver program for the Hall Monitor
 
 # Includes
 import HallHeader
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+from adafruit_ads1x15.ads1x15 import Mode
 
 def HallMonitorDriver():
-    return HallHeader.HALL_MONITOR_ALL_GOOD
+    # Initialize the i2c protocol to interface with the boards
+    i2c = busio.I2C(board.SCL, board.SDA)
 
+    # Initialize the ADC in continuous mode
+    ads = ADS.ADS1015(i2c)
+    ads.mode = Mode.CONTINUOUS
+
+    # Read the channel P0 value and print to the terminal
+    chan = AnalogIn(ads, ADS.P0)
+    print(chan.value, chan.voltage)
+
+    return HallHeader.HALL_MONITOR_ALL_GOOD
